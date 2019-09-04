ba29a87cb7e8d8334d9149cafb5b17b36d466b9a
Nathan Daly <nhdaly@gmail.com>
2019-09-04 09:33:56 -0400

    Reformat whitespace in examples/*.jl

--- examples/fibonacci.jl
+++ examples/fibonacci.jl
@@ -5,31 +5,31 @@ using Select
 import Base.Threads: @spawn
 
 function fibonacci(c::Channel{>:Int}, quit::Channel{>:Int})
-	x, y = 0, 1
-	while true
-		@select begin
-    		c <| x    => begin
-    			x, y = y, x+y
+    x, y = 0, 1
+    while true
+        @select begin
+            c <| x    => begin
+                x, y = y, x+y
             end
-    		quit      => begin
+            quit      => begin
                 println("quit")
                 return
             end
-		end
-	end
+        end
+    end
 end
 
 function main()
     @sync begin
-    	c = Channel{Int}()
-    	quit = Channel{Int}()
-    	@spawn begin
-    		for i in 0:9
-    			println(take!(c))
-    		end
-    		put!(quit, 0)
-    	end
-    	fibonacci(c, quit)
+        c = Channel{Int}()
+        quit = Channel{Int}()
+        @spawn begin
+            for i in 0:9
+                println(take!(c))
+            end
+            put!(quit, 0)
+        end
+        fibonacci(c, quit)
     end
 end
 
